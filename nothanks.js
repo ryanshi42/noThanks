d711350f798819d2f0513670225b1ff07728a637
Ryan Shi <ryan.shi42@gmail.com>
2022-01-17 23:49:41 +1100

    finished refactoring

--- nothanks.js
+++ nothanks.js
@@ -8,50 +8,28 @@ for (var i = minCard; i <= maxCard; i++) {
 }
 let currentCard = 0;
 let chosenCard = 0;
-let p1cards = [];
-let p2cards = [];
-let p3cards = [];
-let p4cards = [];
+let playerCards = [[], [], [], []];
 let activePlayers = 4;
 let gameStatus = false;
 let counters = 11;
-let p1counters = counters;
-let p2counters = counters;
-let p3counters = counters;
-let p4counters = counters;
-let p1sum;
-let p2sum;
-let p3sum;
-let p4sum;
+let playerCounters = [11, 11, 11, 11];
+let playerScores = [0, 0, 0, 0];
 
 function setUp() {
-    p1counters = counters;
-    p2counters = counters;
-    p3counters = counters;
-    p4counters = counters;
     currentPlayer = 1;
     currentCounters = 0;
     availableCards = [];
     for (var i = minCard; i <= maxCard; i++) {
         availableCards.push(i);
     }
-    document.getElementById("p1counters").innerHTML = p1counters;
-    document.getElementById("p2counters").innerHTML = p2counters;
-    document.getElementById("p3counters").innerHTML = p3counters;
-    document.getElementById("p4counters").innerHTML = p4counters;
     document.getElementById("availableCards").innerHTML = availableCards;
-    p1cards = [];
-    p2cards = [];
-    p3cards = [];
-    p4cards = [];
-    document.getElementById("p1cards").innerHTML = p1cards;
-    document.getElementById("p2cards").innerHTML = p2cards;
-    document.getElementById("p3cards").innerHTML = p3cards;
-    document.getElementById("p4cards").innerHTML = p4cards;
-    document.getElementById("p1score").innerHTML = -counters;
-    document.getElementById("p2score").innerHTML = -counters;
-    document.getElementById("p3score").innerHTML = -counters;
-    document.getElementById("p4score").innerHTML = -counters;
+    for (let j = 1; j <= activePlayers; j++) {
+        let pCards = "p" + j.toString() + "cards";
+        document.getElementById(pCards).innerHTML = [];
+        let pScore = "p" + j.toString() + "score";
+        document.getElementById(pScore).innerHTML = -counters;
+
+    }
     document.getElementById("currentCard").innerHTML = "wow, such empty";
     gameStatus = false;
 }
@@ -71,80 +49,27 @@ function startGame() {
 }
 
 function passItOn() {
-    if (currentPlayer == 1) {
-        if (p1counters > 0) {
-            p1counters -= 1;
-            document.getElementById("p1counters").innerHTML = p1counters;
-            updateCurrentCounter("pass");
-            updateScore();
-            incrementPlayer();
-        }
-        else {
-            window.alert("You don't have enough counters!");
-        }
+    if (playerCounters[currentPlayer - 1] > 0) {
+        playerCounters[currentPlayer - 1] -= 1;
+        document.getElementById("p" + currentPlayer.toString() + "counters").innerHTML = playerCounters[currentPlayer - 1];
+        updateCurrentCounter("pass");
+        updateScore();
+        incrementPlayer();
     }
-    else if (currentPlayer == 2) {
-        if (p2counters > 0) {
-            p2counters -= 1;
-            document.getElementById("p2counters").innerHTML = p2counters;
-            updateCurrentCounter("pass");
-            updateScore();
-            incrementPlayer();
-        }
-        else {
-            window.alert("You don't have enough counters!");
-        }
+    else {
+        window.alert("You don't have enough counters!");
     }
-    else if (currentPlayer == 3) {
-        if (p3counters > 0) {
-            p3counters -= 1;
-            document.getElementById("p3counters").innerHTML = p3counters;
-            updateCurrentCounter("pass");
-            updateScore();
-            incrementPlayer();
-        }
-        else {
-            window.alert("You don't have enough counters!");
-        }
-    }
-    else if (currentPlayer == 4) {
-        if (p4counters > 0) {
-            p4counters -= 1;
-            document.getElementById("p4counters").innerHTML = p4counters;
-            updateCurrentCounter("pass");
-            updateScore();
-            incrementPlayer();
-        }
-        else {
-            window.alert("You don't have enough counters!");
-        }
-    }
-    
+        
 }
 
 function takeCard() {
     updateCurrentCounter("take");
 
-    if (currentPlayer == 1) {
-        p1cards.push(currentCard);
-        p1cards.sort((a,b)=>a-b);
-        document.getElementById("p1cards").innerHTML = p1cards;
-    }
-    else if (currentPlayer == 2) {
-        p2cards.push(currentCard);
-        p2cards.sort((a,b)=>a-b);
-        document.getElementById("p2cards").innerHTML = p2cards;
-    }
-    else if (currentPlayer == 3) {
-        p3cards.push(currentCard);
-        p3cards.sort((a,b)=>a-b);
-        document.getElementById("p3cards").innerHTML = p3cards;
-    }
-    else {
-        p4cards.push(currentCard);
-        p4cards.sort((a,b)=>a-b);
-        document.getElementById("p4cards").innerHTML = p4cards;
-    }
+    playerCards[currentPlayer - 1].push(currentCard)
+    playerCards[currentPlayer - 1].sort((a,b)=>a-b);
+    document.getElementById("p" + currentPlayer.toString() + "cards").innerHTML = p1cards;
+
+
     chosenCard = Math.floor(Math.random()*(availableCards.length));
     currentCard = availableCards[chosenCard];
     document.getElementById("currentCard").innerHTML = currentCard;
@@ -169,80 +94,25 @@ function updateCurrentCounter(action) {
         document.getElementById("currentCounters").innerHTML = currentCounters;
     }
     else {
-        if (currentPlayer == 1) {
-            p1counters += currentCounters;
-            document.getElementById("p1counters").innerHTML = p1counters;
-        }
-        else if (currentPlayer == 2) {
-            p2counters += currentCounters;
-            document.getElementById("p2counters").innerHTML = p2counters;
-        }
-        else if (currentPlayer == 3) {
-            p3counters += currentCounters;
-            document.getElementById("p3counters").innerHTML = p3counters;
-        }
-        else {
-            p4counters += currentCounters;
-            document.getElementById("p4counters").innerHTML = p4counters;
-        }
+        playerCounters[currentPlayer - 1] += currentCounters;
+        document.getElementById("p" + currentPlayer.toString() + "counters").innerHTML = playerCounters[currentPlayer - 1];
         currentCounters = 0;
         document.getElementById("currentCounters").innerHTML = currentCounters;
     }
 }
 
 function updateScore() {
-    if (currentPlayer == 1) {
-        p1sum = 0;
-        for (var j = p1cards.length - 1; j >= 0; j--) {
-            if (j == 0) {
-                p1sum += p1cards[j];
-            }
-            else if (p1cards[j-1] + 1 != p1cards[j]) {
-                p1sum += p1cards[j];
-            }
+    currScore = 0;
+    for (var j = playerCards[currentPlayer - 1].length - 1; j >= 0; j--) {
+        if (j == 0) {
+            currScore += playerCards[currentPlayer - 1][j];
         }
-        p1sum -= p1counters;
-        document.getElementById("p1score").innerHTML = p1sum;
-    }
-    else if (currentPlayer == 2) {
-        p2sum = 0;
-        for (var j = p2cards.length - 1; j >= 0; j--) {
-            if (j == 0) {
-                p2sum += p2cards[j];
-            }
-            else if (p2cards[j-1] + 1 != p2cards[j]) {
-                p2sum += p2cards[j];
-            }
+        else if (playerCards[currentPlayer - 1][j-1] + 1 != playerCards[currentPlayer - 1][j]) {
+            currScore += playerCards[currentPlayer - 1][j];
         }
-        p2sum -= p2counters;
-        document.getElementById("p2score").innerHTML = p2sum;
     }
-    else if (currentPlayer == 3) {
-        p3sum = 0;
-        for (var j = p3cards.length - 1; j >= 0; j--) {
-            if (j == 0) {
-                p3sum += p3cards[j];
-            }
-            else if (p3cards[j-1] + 1 != p3cards[j]) {
-                p3sum += p3cards[j];
-            }
-        }
-        p3sum -= p3counters;
-        document.getElementById("p3score").innerHTML = p3sum;
-    }
-    else if (currentPlayer == 4) {
-        p4sum = 0;
-        for (var j = p4cards.length - 1; j >= 0; j--) {
-            if (j == 0) {
-                p4sum += p4cards[j];
-            }
-            else if (p4cards[j-1] + 1 != p4cards[j]) {
-                p4sum += p4cards[j];
-            }
-        }
-        p4sum -= p4counters;
-        document.getElementById("p4score").innerHTML = p4sum;
-    }
+    currScore -= playerCounters[currentPlayer - 1];
+    document.getElementById("p" + currentPlayer.toString() + "score").innerHTML = currScore;
 }
 
 function updateActivePlayers(a) {
@@ -253,6 +123,32 @@ function updateActivePlayers(a) {
     if (a == 0) {
         if (activePlayers < 8) {
             activePlayers += 1;
+            // TODO:
+            playerCards.push([]);
+            playerScores.push(0);
+            playerCounters.push(11);
+
+            var th = document.createElement("th");
+            th.innerText = "Player " + activePlayers.toString() + ": ";
+            var inp = document.createElement("input");
+            inp.setAttribute("placeholder", "bobsmiley");
+            th.appendChild(inp);
+            document.getElementById("displayPlayerNames").appendChild(th);
+
+            var td = document.createElement("td");
+            td.id = "p" + activePlayers.toString() + "counters";
+            document.getElementById("displayCountersRemaining").appendChild(td);
+
+            var td1 = document.createElement("td");
+            td1.id = "p" + activePlayers.toString() + "cards";
+            document.getElementById("displayCardsRemaining").appendChild(td1);
+
+            var td2 = document.createElement("td");
+            td2.id = "p" + activePlayers.toString() + "score";
+            td2.innerHTML = -11;
+            document.getElementById("displayCurrentScores").appendChild(td2);
+
+            
         }
         else {
             window.alert("You have already reached the maximum number of players!");
@@ -261,6 +157,21 @@ function updateActivePlayers(a) {
     else {
         if (activePlayers > 2) {
             activePlayers -= 1;
+            playerCards.remove(-1);
+            playerScores.remove(-1);
+            playerCounters.remove(-1);
+
+            document.getElementById("displayPlayerNames") = playerNames;
+            playerNames.removeChild(playerNames.lastChild);
+
+            document.getElementById("displayCountersRemaining") = playerCounters;
+            playerCounters.removeChild(playerCounters.lastChild);
+
+            document.getElementById("displayCardsRemaining") = playerCards;
+            playerCards.removeChild(playerNames.lastChild);
+
+            document.getElementById("displayCurrentScores") = playerScores;
+            playerScores.removeChild(playerScores.lastChild);
         }
         else {
             window.alert("You have already reached the minimum number of players!");
@@ -280,15 +191,11 @@ function updateCounters(a) {
     else {
         counters -= 1;
     }
-    p1counters = counters;
-    p2counters = counters;
-    p3counters = counters;
-    p4counters = counters;
+    
     document.getElementById("counters").innerHTML = counters;
-    document.getElementById("p1counters").innerHTML = counters;
-    document.getElementById("p2counters").innerHTML = counters;
-    document.getElementById("p3counters").innerHTML = counters;
-    document.getElementById("p4counters").innerHTML = counters;
+    for (let i = 1; i <= activePlayers; i++) {
+        document.getElementById("p" + i.toString() + "counters").innerHTML = counters;
+    }
 }
 
 // Take out 5 cards at the start
